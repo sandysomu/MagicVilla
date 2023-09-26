faa74d7df67aab30b63472b0a4cde46b634e397e
sandysomu <ssingh17@woolworths.com.au>
2023-09-26 10:38:38 +1000

    First API added

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,6 +1,25 @@
-﻿namespace MagicVilla_VillaAPI.Controllers
+﻿using MagicVilla_VillaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
 {
-    public class VillaAPIController
+    [Route("Api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : Controller
     {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa> { 
+                new Villa { Id=1, name= "Pool View"},
+                new Villa { Id=2,name= "Beach View"}
+            };
+        }
+
+
+
+
+
+        
     }
 }
